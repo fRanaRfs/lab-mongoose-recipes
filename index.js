713b3112431db1eb8da4242f73061693905037d7
fRanaRfs <franarfs.bureau@gmail.com>
2022-06-23 17:31:53 +0200

    Lab finish
    Finish

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+
+const MONGODB_URI = 'mongodb://localhost:27017';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +19,46 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+    title: 'Pizza Hawaiana',
+    cuisine: 'Mediterranean'  
+    })
   })
+  .then((result) => {
+    console.log(result.title)
+  })
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then((allRecipes) => {
+    let allRecipesTitle = allRecipes.map((recipeTitle) => {
+      return recipeTitle.title;
+    })
+    console.log(allRecipesTitle)
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate( 
+      {title: 'Rigatoni alla Genovese'},
+      {duration: 100},
+      {new: true}
+    )
+  })
+  .then (() => {
+    console.log('Succes')
+  })
+  .then (() => {
+    return Recipe.deleteOne(
+      {title:'Carrot Cake'},
+    )
+  })
+  .then (() => {
+    console.log('Succes')
+  })
+
+  .then (() => {
+    mongoose.connection.close()
+  })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
